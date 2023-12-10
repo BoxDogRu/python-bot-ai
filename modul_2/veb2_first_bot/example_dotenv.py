74b12bbe943dadce2926559932a81637644f7b6d
Vladimir Rudenko <ibrainmod@gmail.com>
2023-12-10 17:24:27 +0300

    veb 2.2 - first bot

--- modul_2/veb2_first_bot/example_dotenv.py
+++ modul_2/veb2_first_bot/example_dotenv.py
@@ -1,4 +1,6 @@
 # pip install python-dotenv
+# необходимо создать файл .env, в котором и будем хранить конфиденциальные данные, например, токен бот
+# необходимо проверить, что .env не пушится в git
 
 from dotenv import load_dotenv
 import os
