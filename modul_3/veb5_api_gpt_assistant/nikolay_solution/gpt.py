64481034a3e3ec3cdcc0b05807dd2819350efd42
Vladimir Rudenko <ibrainmod@gmail.com>
2024-02-22 17:40:10 +0300

    veb 3.5. api gpt assistant

---
+++ modul_3/veb5_api_gpt_assistant/nikolay_solution/gpt.py
@@ -0,0 +1,74 @@
+import requests
+from transformers import AutoTokenizer
+
+
+class GPT:
+    def __init__(self, system_content="Ты - дружелюбный помощник для решения задач по математике. "
+                                      "Давай подробный ответ с решением на русском языке."):
+        self.system_content = system_content
+        self.URL = 'http://localhost:1234/v1/chat/completions'
+        self.HEADERS = {"Content-Type": "application/json"}
+        self.MAX_TOKENS = 100
+        self.assistant_content = "Решим задачу по шагам: "
+
+    # Подсчитываем количество токенов в промпте
+    @staticmethod
+    def count_tokens(prompt):
+        tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")  # название модели
+        return len(tokenizer.encode(prompt))
+
+    # Проверка ответа на возможные ошибки и его обработка
+    def process_resp(self, response) -> [bool, str]:
+        # Проверка статус кода
+        if response.status_code < 200 or response.status_code >= 300:
+            self.clear_history()
+            return False, f"Ошибка: {response.status_code}."
+
+        # Проверка на json
+        try:
+            full_response = response.json()
+        except:
+            self.clear_history()
+            return False, "Ошибка получения JSON"
+
+        # Проверка ответа на ошибку
+        if "error" in full_response or 'choices' not in full_response:
+            self.clear_history()
+            return False, f"Ошибка: {full_response}"
+
+        # Результат
+        result = full_response['choices'][0]['message']['content']
+
+        if result == "":
+            result = "Объяснение закончено."
+        # Пустой результат == объяснение закончено
+
+        # Сохраняем ответ в истории
+        self.save_history(result)
+        return True, self.assistant_content
+
+    # Формирование промпта
+    def make_prompt(self, user_request):
+        json = {
+            "messages": [
+                {"role": "system", "content": self.system_content},
+                {"role": "user", "content": user_request},
+                {"role": "assistant", "content": self.assistant_content}
+            ],
+            "temperature": 1.2,
+            "max_tokens": self.MAX_TOKENS,
+        }
+        return json
+
+    # Отправка запроса
+    def send_request(self, json):
+        resp = requests.post(url=self.URL, headers=self.HEADERS, json=json)
+        return resp
+
+    # Сохраняем историю ответов
+    def save_history(self, content_response):
+        self.assistant_content += content_response
+
+    # Очистка истории ответов
+    def clear_history(self):
+        self.assistant_content = "Решим задачу по шагам: "
