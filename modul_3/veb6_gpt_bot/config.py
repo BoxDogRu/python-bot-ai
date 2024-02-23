bd650de6b1b696adf7b59a9ebc1f1071a1cb0ec0
Vladimir Rudenko <ibrainmod@gmail.com>
2024-02-24 01:04:57 +0300

    veb 3.6. gpt bot

--- modul_3/veb6_gpt_bot/config.py
+++ modul_3/veb6_gpt_bot/config.py
@@ -1,8 +1,10 @@
+# Загружать файлы с доступами, токенами, паролями в гит - путь к провалу!
+# Следите за безопасностью!
+TOKEN = "6657398815:AAEb90Rxks38lnlNYCYwJOAvSKIbdcEJkuQ"
+
 GPT_LOCAL_URL = 'http://localhost:1234/v1/chat/completions'
 GPT_WEB_URL = 'обратиться к наставнику'
 
 HEADERS = {"Content-Type": "application/json"}
-
-TOKEN = "6657398815:AAEb90Rxks38lnlNYCYwJOAvSKIbdcEJkuQ"
 
 MAX_TOKENS = 150
