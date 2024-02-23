2473c46a80c41bf2cc35cf921ab473d99c3e2359
Vladimir Rudenko <ibrainmod@gmail.com>
2024-02-24 00:51:36 +0300

    veb 3.6. gpt bot

---
+++ modul_3/veb6_gpt_bot/config.py
@@ -0,0 +1,8 @@
+GPT_LOCAL_URL = 'http://localhost:1234/v1/chat/completions'
+GPT_WEB_URL = 'обратиться к наставнику'
+
+HEADERS = {"Content-Type": "application/json"}
+
+TOKEN = "6657398815:AAEb90Rxks38lnlNYCYwJOAvSKIbdcEJkuQ"
+
+MAX_TOKENS = 150
